cca7ad04dabc4b1b15c04783c6f5d501d2ace90e
Brandon Gomez M <bran_gomon25@hotmail.com>
2020-03-21 10:23:45 -0600

    add new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    ArticlesComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  private options: Array<boolean> = [true, false, false, false];
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  desactiveShows(){
+    console.log(this.options);
+    for (let index = 0; index < this.options.length; index++) {
+      this.options[index] = false;
+    }
+    console.log(this.options);
   }
+
+  whatIsIt(){
+    this.desactiveShows();
+    this.options[1] = true;
+  }
+
+  symptom(){
+    this.desactiveShows();
+    this.options[2] = true;
+  }
+
+  transmission(){
+    this.desactiveShows();
+    this.options[3] = true;
+  }
+  
 
 }
