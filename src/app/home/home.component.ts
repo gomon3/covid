6be983b8416063f8fa9d83c12ab897a67b5d82b0
Brandon Gomez M <bran_gomon25@hotmail.com>
2020-03-23 17:49:54 -0600

    New elements

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,8 +13,36 @@ export function getAccordionConfig(): AccordionConfig {
 })
 export class HomeComponent implements OnInit {
 
+  options: Array<boolean> = [false, true, false, false];
+
   constructor() { }
 
   ngOnInit() { }
+
+  hideAll(){
+    for (let i = 0; i < this.options.length; i++) {
+      this.options[i] = false;
+    }
+  }
+
+  outstandingNews() {
+    this.hideAll();
+    this.options[0] = true;
+  }
+
+  covid() {
+    this.hideAll();
+    this.options[1] = true;
+  }
+
+  statistics() {
+    this.hideAll();
+    this.options[2] = true;
+  }
+
+  otherActions() {
+    this.hideAll();
+    this.options[3] = true;
+  }
 
 }
